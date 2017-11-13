8de1294c7a5800e044bddf0405b19fc7785b520a
Corin Faife <corin.faife@gmail.com>
2017-11-13 16:01:44 -0500

    First framework build

---
+++ src/js/app.js
@@ -0,0 +1,15 @@
+/* Vue JS */
+var app = new Vue({
+    el: '#vue',
+    data: {
+      message: 'VueJS: on.'
+    }
+})
+
+
+/*
+//Load a JSON file
+var jsonData = $.getJSON('./src/data/somedata.json', function(data){
+  doSomethingWith(data)
+});
+*/
